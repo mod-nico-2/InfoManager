7f17bb58267f25020f4c3770c9e17aa41c0a25bf
Vespertino <Vespertino@C09PC08.informatica.comercio>
2025-02-13 21:06:06 +0100

    Inicio proyecto api

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DesignAPI.Models.Entity;
+
+namespace DesignAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //Add models here
+        public DbSet<User> Users { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ API/Models/DTOs/UserDto/UserLoginResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using DesignAPI.Models.Entity;
+
+namespace DesignAPI.Models.DTOs.UserDto
+{
+    public class UserLoginResponseDto
+    {
+        public AppUser User { get; set; }
+        public string Token { get; set; }
+
+    }
+}
